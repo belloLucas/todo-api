58bc05e20f7804e9603e966f700e80378714539e
belloLucas <lucasbellodev@gmail.com>
2024-01-20 19:09:19 -0300

    chore: Fixes on project structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import todoRouter from "./routes/todoRouter";
+import todoRouter from "./routes/todoRoute";
 
 dotenv.config();
 
