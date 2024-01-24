e1f6dba8daf5cc857ce7592f27e22312e79e8c0b
belloLucas <lucasbellodev@gmail.com>
2024-01-24 17:38:32 -0300

    feat: configuring cors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import todoRouter from "./routes/todoRoute";
 
@@ -7,6 +8,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
